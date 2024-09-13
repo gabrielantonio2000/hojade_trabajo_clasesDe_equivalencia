8bd6b45321a149829ab24239fd4465da56412b58
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-09-13 17:52:28 -0600

    modificando mi funcion

--- src/operaciones.js
+++ src/operaciones.js
@@ -6,24 +6,28 @@ export const multiplicacion = (a, b) => a * b;
 
 export const division = (a, b) => {
   if (b === 0) {
-    throw new Error('No se puede dividir por 0');
+    throw new Error("No se puede dividir por 0");
   }
-
   return a / b;
-
-  
 };
 
-
+// Función para determinar la aprobación de un préstamo
 export const determinarAprobacionPrestamo = (tipo, deudas) => {
   if (tipo === "E") {
-    return "El préstamo no está aprobado.";
-  } else if (tipo === "T") {
-    return "El préstamo está aprobado.";
-  } else if (tipo === "R") {
-    return deudas
+    // Estudiante
+    return deudas === false
       ? "El préstamo no está aprobado."
-      : "El préstamo está aprobado.";
+      : "El préstamo no está aprobado.";
+  } else if (tipo === "T") {
+    // Trabajador
+    return deudas === false
+      ? "El préstamo está aprobado."
+      : "El préstamo no está aprobado.";
+  } else if (tipo === "R") {
+    // Retirado
+    return deudas === false
+      ? "El préstamo está aprobado."
+      : "El préstamo no está aprobado.";
   }
   return "Tipo de solicitante inválido. Las opciones válidas son: E (Estudiante), T (Trabajador), R (Retirado).";
 };
